b481974d17f338e5a41d2148d15c45bdab104226
mandil01 <juan.castro03@iest.edu.mx>
2026-02-06 07:39:30 -0600

    Add files via upload
    se agregaron las operaciones de resta division y multiplicación

---
+++ pp4/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x As Integer = Integer.Parse(TextBox1.Text)
+        Dim y As Integer = Integer.Parse(TextBox2.Text)
+        Dim r As Integer = x + y
+        Label4.Text = r.ToString()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim x As Integer = Integer.Parse(TextBox1.Text)
+        Dim y As Integer = Integer.Parse(TextBox2.Text)
+        Dim r As Integer = x - y
+        Label4.Text = r.ToString()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim x As Integer = Integer.Parse(TextBox1.Text)
+        Dim y As Integer = Integer.Parse(TextBox2.Text)
+        Dim r As Integer = x * y
+        Label4.Text = r.ToString()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim x As Integer = Integer.Parse(TextBox1.Text)
+        Dim y As Integer = Integer.Parse(TextBox2.Text)
+        Dim r As Decimal = x / y
+        Label4.Text = r.ToString()
+    End Sub
+End Class
